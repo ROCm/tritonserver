d24988272f1a439736ac9f9d0567042ed225f1ea
linsun12 <linsun12@amd.com>
2026-02-19 15:01:34 -0500

    rename repos to triton-inference-server-xx-legacy

--- build.py
+++ build.py
@@ -2138,14 +2138,14 @@ def backend_build(
         flashattn_build(cmake_script,build_dir)
         cmake_script.cmd("git clone https://github.com/vllm-project/vllm.git vllm".format(tag))
     elif be == "pytorch" and FLAGS.enable_rocm:
-        cmake_script.gitclone("triton-inference-server-pytorch_backend", tag, be, github_organization)
+        cmake_script.gitclone("triton-inference-server-pytorch_backend-legacy", tag, be, github_organization)
     elif be == "python" and FLAGS.enable_rocm:
         # Use AMD-specific python_backend fork for ROCm support
         cmake_script.gitclone(
             "triton-inference-server-python_backend", "r23.10-amd-port", "python", "https://github.com/ROCm")
     elif (be == "onnxruntime") and (FLAGS.enable_rocm):
         cmake_script.gitclone(
-            "triton-inference-server-onnxruntime_backend", "rocm7.1.1_ort1.23", "onnxruntime_backend", "https://github.com/ROCm")
+            "triton-inference-server-onnxruntime_backend-legacy", "rocm7.1.1_ort1.23", "onnxruntime_backend", "https://github.com/ROCm")
     else:
         cmake_script.gitclone(backend_repo(be), tag, be, github_organization)
 
